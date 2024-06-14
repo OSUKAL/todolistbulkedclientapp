9703bc8e65199ebfd50180e841dd404a40fcf449
OSUKAL <floky798@gmail.com>
2024-06-15 02:46:46 +1000

    Модель пользователя с изменениями

---
+++ src/models/User/EditUserModel.ts
@@ -0,0 +1,11 @@
+﻿import { UserRole } from '../../Enums/UserRole.ts'
+
+/**Модель пользователя с изменениями*/
+export type EditUserModel = {
+    /**Идентификатор*/
+    id:string
+    /**Имя пользователя*/
+    username: string
+    /**Роль*/
+    role: UserRole
+}
